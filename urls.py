b77bad439da15d2e87402ffc2c813977f5e0a12a
Anastasia-Albertovna-Andreeva <anastasia.albertovna.andreeva@gmail.com>
2024-09-22 20:31:35 +0200

    changes

---
+++ urls.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+class Urls:
+    MAIN_PAGE = 'https://qa-scooter.praktikum-services.ru/'
+    ORDER_PAGE = 'https://qa-scooter.praktikum-services.ru/order'
+    ORDER_STATUS_PAGE = 'https://qa-scooter.praktikum-services.ru/track'
+    DZEN_HOME_PAGE = 'dzen.ru'
+    YANDEX_HOME_PAGE = 'ya.ru'
